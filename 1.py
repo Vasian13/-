3391f3dcadb325106acd1e2cc5a7afa057a29e64
Никитин Андрей <128966776+Vasian13@users.noreply.github.com>
2023-04-26 22:01:20 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,17 @@
-def f(())
+squares = []
+for num in range(3, 12):
+    squares += [num*2]
+# print(squares)
+
+
+# squares_gen = (num*2 for num in range(3, 12))
+# a = list(squares_gen)
+# b = list(squares_gen)
+
+# print(a, b, sep='\n')
+
+
+hobbies = ['football', 'hokhey', 'basketball']
+print("My hobbies are: ")
+print(' '.join(hobbies))
+print(hobbies)
