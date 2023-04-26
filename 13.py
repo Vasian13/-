8b2d85ae55a90c4292c08e12f054d83f0db3d1a6
Nikitin Andrey <andreynikitin@bk.ru>
2023-04-26 23:38:38 +0300

    'second'

---
+++ 13.py
@@ -0,0 +1,17 @@
+squares = []
+for num in range(3, 12):
+    squares += [num*2]
+# print(squares)
+
+
+# squares_gen = (num*2 for num in range(3, 12))
+# a = list(squares_gen)
+# b = list(squares_gen)
+
+# print(a, b, sep='\n')
+
+
+hobbies = ['football', 'hokhey', 'basketball']
+print("My hobbies are: ")
+print(' '.join(hobbies))
+print(hobbies)
